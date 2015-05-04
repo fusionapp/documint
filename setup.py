06b3c518b11e42248a3251e76712eb8211747b91
Tristan Seligmann <mithrandi@mithrandi.net>
2015-05-04 05:40:56 +0200

    Handle unclean restarts better.

--- setup.py
+++ setup.py
@@ -4,7 +4,16 @@ from setuptools import find_packages, setup
 
 setup(
     name="Documint",
-    version="1.0",
+    version="15.5.0",
+    url="https://github.com/fusionapp/documint",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Markup :: HTML',
+        ],
     packages=find_packages() + ['twisted.plugins'],
     install_requires=['Twisted', 'lxml'],
     include_package_data=True)
